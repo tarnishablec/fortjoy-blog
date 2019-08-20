3ed481b028d3d31a2b8088d3424ffcf88d507022
tarnishablec <tarnishablec@outlook.com>
2019-08-21 00:58:06 +0800

    Initial commit

---
+++ src/main/java/com/tarnis/fortjoyblog/FortjoyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.tarnis.fortjoyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortjoyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortjoyBlogApplication.class, args);
+	}
+
+}
